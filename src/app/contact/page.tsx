70e542916d4135d4c92b156ffed0717512ab67a3
Kevin <kevin.ortiz.software@gmail.com>
2025-12-04 12:15:56 -0500

    uploading page.tsx for contact route and pushing the .css for the project

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,44 +1,34 @@
 import React from "react";
 import "./contact.css";
-import emailIcon from "@/Assets/email.png"
-import linkedInImage from "@/Assets/linkedin.png"
-
-
+import emailIcon from "@/Assets/email.png";
+import linkedInImage from "@/Assets/linkedin.png";
 
 export default function contact() {
   return (
-  <>
-  
-  
-    
-    <section id="contact">
-        <h1 className="title">Contact Me</h1>
+    <>
+      <section id="contact">
         <div className="contact-info-upper-container">
-            <div className="contact-info-container">
+          <div className="contact-info-container">
+            <a href="mailto:kevin.ortiz.software@gmail.com?subject=Contact">
+              <img src={emailIcon.src} alt="email icon" className="icon" />
+            </a>
 
-                <a href="mailto:kevin.ortiz.software@gmail.com?subject=Contact">
-                    <img src={emailIcon.src} alt="email icon" className="icon" />
-                </a>
+            <p>Kevin.ortiz.software&#64;gmail.com</p>
+          </div>
 
-                <p>
-                    Kevin.ortiz.software&#64;gmail.com
-                </p>
-            </div>
+          <div className="contact-info-container">
+            <a href="https://www.linkedin.com" target="_blank">
+              <img
+                src={linkedInImage.src}
+                alt="My LinkedIn profile"
+                className="icon"
+              />
+            </a>
 
-            <div className="contact-info-container">
-                <a href="https://www.linkedin.com" target="_blank">
-                    <img src={linkedInImage.src} alt="My LinkedIn profile" className="icon" />
-                </a>
-
-                <p>
-                    LinkedIn
-                </p>
-            </div>
+            <p>LinkedIn</p>
+          </div>
         </div>
-    </section>
-
-  
-  
-  </>
+      </section>
+    </>
   );
 }
